80274fe093542e7b5d1f34ff0572401404d70cbf
MajdBarakat <78052909+MajdBarakat@users.noreply.github.com>
2021-08-25 04:16:56 +0100

    Installed JQuery w npm to make life easier with js functions

---
+++ app.js
@@ -0,0 +1,7 @@
+var menuBtn = document.getElementsByClassName('burger');
+
+var navMenu = document.getElementsByClassName('nav-menu');
+
+function menuButtonClick() {
+    navMenu.[0].style.display = "flex";
+};
