1d3fab27c87afe5fc0c6e6c1b7f0b59ada2f3c55
MajdBarakat <78052909+MajdBarakat@users.noreply.github.com>
2021-08-26 05:09:33 +0100

    added jquery js scripts created some animations and added scrolling

--- app.js
+++
@@ -1,7 +0,0 @@
-var menuBtn = document.getElementsByClassName('burger');
-
-var navMenu = document.getElementsByClassName('nav-menu');
-
-function menuButtonClick() {
-    navMenu.[0].style.display = "flex";
-};
---
+++ js/app.js
@@ -0,0 +1,22 @@
+// $(".burger").click(function(){
+//     var burger = $(".burger");
+//     var line1 = $(".line1");
+//     var line2 = $(".line2");
+//     var line3 = $(".line3");
+
+//     if(burger.attr("id") == "OFF"){
+//         burger.attr("id","ON")
+//         line1.transition({rotate:'45', y:"21px"})
+//         line2.transition({x:"-50px",opacity:0})
+//         line3.transition({rotate:'-45deg' , y:"-21px"})
+//         $(".nav-menu").transition({x:'0vw'});
+//     }
+//     else{
+//         burger.attr("id","OFF")
+//         $(".nav-menu").transition({x:'100vw'});
+//         line1.transition({rotate:'-0', y:"0"})
+//         line2.transition({x:"0",opacity:1})
+//         line3.transition({rotate:'0' , y:"0"})
+//     }
+// });
+
