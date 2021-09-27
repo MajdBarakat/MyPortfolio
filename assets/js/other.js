c24c9ec25fffe7204213d9311c0bbc7ca8cb4c6f
MajdBarakat <78052909+MajdBarakat@users.noreply.github.com>
2021-09-27 14:43:38 +0100

    organised files, changed clicking on my email to copying to clipboard (need indicator)

---
+++ assets/js/other.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    
+    var copyText = document.getElementById("textCopy");
+    
+    $(window).scroll(function(){
+        var scrollBarLocation = $(window).scrollTop()
+        if(scrollBarLocation > 100){
+            $(".navbar-container").addClass("active");
+            console.log(scrollBarLocation)
+        }
+        else{
+            console.log("should delete")
+            $(".navbar-container").removeClass("active");
+        }
+    })
+
+    $(".my-email").click(function(){
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        navigator.clipboard.writeText(copyText.value);
+        
+    })
+    
+    $(".light-dark-modes").click(function(){
+        console.log("clicked")
+        if($("#dark-btn").hasClass("active") == true){
+            $("html").removeClass("dark")
+            $("html").addClass("light")
+            $("#light-btn").addClass("active");
+            $("#dark-btn").removeClass("active")
+        }else{
+            $("html").removeClass("light")
+            $("html").addClass("dark")
+            $("#dark-btn").addClass("active");
+            $("#light-btn").removeClass("active")
+        }
+    });
+
+    
+  
+    
+});
