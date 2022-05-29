12685464160d585457ea2cc46c3fa916488266a7
Majd Barakat <78052909+MajdBarakat@users.noreply.github.com>
2022-05-29 17:09:08 +0100

    Updated portfolio
    updated portfolio with appropriate changes, and changed format using prettier. Added alert on clicking my email to show it has been copied.

--- assets/js/other.js
+++ assets/js/other.js
@@ -1,42 +1,36 @@
-$(document).ready(function(){
-    
-    var copyText = document.getElementById("textCopy");
-    
-    $(window).scroll(function(){
-        var scrollBarLocation = $(window).scrollTop()
-        if(scrollBarLocation > 100){
-            $(".navbar-container").addClass("active");
-            console.log(scrollBarLocation)
-        }
-        else{
-            console.log("should delete")
-            $(".navbar-container").removeClass("active");
-        }
-    })
+$(document).ready(function () {
+  var copyText = document.getElementById("textCopy");
 
-    $(".my-email").click(function(){
-        copyText.select();
-        copyText.setSelectionRange(0, 99999);
-        navigator.clipboard.writeText(copyText.value);
-        
-    })
-    
-    $(".light-dark-modes").click(function(){
-        console.log("clicked")
-        if($("#dark-btn").hasClass("active") == true){
-            $("html").removeClass("dark")
-            $("html").addClass("light")
-            $("#light-btn").addClass("active");
-            $("#dark-btn").removeClass("active")
-        }else{
-            $("html").removeClass("light")
-            $("html").addClass("dark")
-            $("#dark-btn").addClass("active");
-            $("#light-btn").removeClass("active")
-        }
-    });
+  $(window).scroll(function () {
+    var scrollBarLocation = $(window).scrollTop();
+    if (scrollBarLocation > 100) {
+      $(".navbar-container").addClass("active");
+      console.log(scrollBarLocation);
+    } else {
+      console.log("should delete");
+      $(".navbar-container").removeClass("active");
+    }
+  });
 
-    
-  
-    
+  $(".my-email").click(function () {
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value);
+    window.alert("Copied to Clipboard!");
+  });
+
+  $(".light-dark-modes").click(function () {
+    console.log("clicked");
+    if ($("#dark-btn").hasClass("active") == true) {
+      $("html").removeClass("dark");
+      $("html").addClass("light");
+      $("#light-btn").addClass("active");
+      $("#dark-btn").removeClass("active");
+    } else {
+      $("html").removeClass("light");
+      $("html").addClass("dark");
+      $("#dark-btn").addClass("active");
+      $("#light-btn").removeClass("active");
+    }
+  });
 });
